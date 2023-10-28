7bcbe779b8edd237c321f7b36773bc83d6e56177
AVVKavvk <kumawatvipin066@gmail.com>
2023-10-28 13:33:59 +0530

    use seeders

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 const express = require("express");
+const sequelize = require("sequelize");
 const { PORT } = require("./config/serverConfig");
 const apiRoute = require("./routes/index");
+const { Airport, City } = require("./models/index");
+const db = require("./models");
 function startServer() {
   const app = express();
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     app.use(express.json({ limit: "10mb" }));
     app.use("/api", apiRoute);
+
     console.log(`server is running on port ${PORT}`);
+    if (process.env.SYNC_DB) {
+      db.sequelize.sync({ alter: true });
+    }
+    // const airports = await Airport.findAll({
+    // include: City,
+    //   include: [{ model: City }],
+    // });
+    // console.log(airports);
+    // const newAirport = await Airport.create({
+    //   name: "Mumbai Airport",
+    //   cityId: 6,
+    // });
+    // console.log(newAirport);
+    // const city = await City.findOne({
+    //   where: {
+    //     id: 7,
+    //   },
+    // });
+    //   const newAirport = await Airport.findOne({
+    //     where: {
+    //       name: "Mumbai Airport",
+    //     },
+    //   });
+    //   await city.addAirports(newAirport);
+    // const airports = await city.getAirports();
+    // console.log(airports);
   });
 }
 startServer();
